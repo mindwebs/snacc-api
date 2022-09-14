fdd9e16c180f5c87348e7453de5133420a0dc723
singhayushh <coding.ayush@gmail.com>
2022-09-14 14:17:35 +0530

    response message updated

--- src/middlewares/authentication.middleware.ts
+++ src/middlewares/authentication.middleware.ts
@@ -6,7 +6,7 @@ export const Auth = async (req: Request, res: Response, next: NextFunction) => {
     try {
         // step 1 - fetch token from the header
         const token = req.header("Authorization")?.replace("Bearer ", "");
-        if (!token) return res.status(403).json({ "message": "token does not exist in req header" });
+        if (!token) return res.status(403).json({ "message": "Token does not exist in req header" });
 
         // step 2 - use jwt.verify() to unhash the token using the initial JWT_SECRET we had used to hash it.
         const decodedPayload: JWTPayload = jwt.verify(token, String(process.env.JWT_SECRET)) as JWTPayload;
